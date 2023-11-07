f88b486f2d7faa58ecd29afc602e4885c43997de
Kindred <lenatthomas16@gmail.com>
2023-11-07 21:37:11 +0530

    Updated Lab Cycle 2

--- 1.R
+++
@@ -1,4 +0,0 @@
-getwd()
-IrisData = read.csv('iris.csv')
-png("IrisScatter.png", width = 1000 , height = 500 , res=100)
-plot(IrisData$sepal_length , IrisData$petal_length , xlab = 'Sepal Length',  ylab = 'Petal Length' , main = 'Sepal Length vs Petal Length')
